110f3388814e3a29c979e625b8561a893683caf0
Vadim <frogix@ya.ru>
2021-01-11 17:51:57 +0500

    replaces single quotes with double quotes in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,43 @@
-const path = require('path');
+const path = require("path");
 
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
-    plugins: [
-        // new CleanWebpackPlugin(),
-        new CleanWebpackPlugin({cleanStaleWebpackAssets: false}),
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, "src", "index.pug")
-        }),
+  mode: "development",
+  entry: "./src/index.js",
+  plugins: [
+    // new CleanWebpackPlugin(),
+    new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src", "index.pug"),
+    }),
+  ],
+  devServer: {
+    contentBase: "./dist",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.pug$/i,
+        loader: "pug-loader",
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
     ],
-    devServer: {
-        contentBase: './dist',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.s[ac]ss$/i,
-                use: ['style-loader', 'css-loader', 'sass-loader'],
-            },
-            {
-                test: /\.pug$/i,
-                loader: 'pug-loader'
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif|)$/i,
-                type: 'asset/resource',
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/i,
-                type: 'asset/resource',
-            },
-        ],
-    },
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
 };
